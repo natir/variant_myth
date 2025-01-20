50e87f0ff58ce0019f25d03a549e9d758a21f0a1
Charles Monod-Broca <charles.monod-broca@neuf.fr>
2025-01-20 14:09:56 +0100

    Fixes #4
    - Added tests for JsonWriter
    
    -Implemented Json and NDJson

--- src/output/json.rs
+++ src/output/json.rs
@@ -4,6 +4,10 @@
 
 /* crate use */
 
+use std::collections::BTreeMap;
+
+use serde_json::json;
+
 /* project use */
 use crate::error;
 use crate::myth;
@@ -13,6 +17,12 @@ use crate::output;
 pub struct JsonWriter<W> {
     output_stream: W,
     json_format: JsonFormat,
+    write_state: WriteState,
+}
+
+enum WriteState {
+    WroteMetadata,
+    WroteRecord,
 }
 
 /// Choose output JSON format
@@ -28,52 +38,65 @@ pub enum JsonFormat {
     NdJson,
 }
 
-fn get_metadata_pretty() -> Vec<String> {
-    crate::output::get_metadata()
-        .iter()
-        .filter_map(|(k, v)| serde_json::to_string_pretty(&(k, v)).ok())
-        .collect()
-}
-
-fn get_metadata() -> Vec<String> {
-    crate::output::get_metadata()
-        .iter()
-        .filter_map(|(k, v)| serde_json::to_string(&(k, v)).ok())
-        .collect()
+fn get_metadata() -> serde_json::Value {
+    let mut map = BTreeMap::new();
+    for (k, v) in crate::output::get_metadata() {
+        map.entry(k).or_insert(v);
+    }
+    json!(map)
 }
 
 impl<W: std::io::Write> JsonWriter<W> {
     /// Create a new JsonWriter
     pub fn new(mut output_stream: W, json_format: JsonFormat) -> error::Result<Self> {
+        let metadata_repr = json!(get_metadata());
         if json_format == JsonFormat::Json {
-            output_stream.write(b"{\n")?;
-            let metadata_repr = serde_json::to_string_pretty(&get_metadata_pretty())?;
-            output_stream.write(metadata_repr.as_bytes())?;
+            output_stream.write(format!("{{\n\"metadata\":\n{:#}", metadata_repr).as_bytes())?;
         } else {
-            let metadata_repr = serde_json::to_string(&get_metadata())?;
-            output_stream.write(metadata_repr.as_bytes())?;
+            output_stream.write(format!("{{\"metadata\":{}}}", metadata_repr).as_bytes())?;
+        }
+        if json_format == JsonFormat::Json {
+            output_stream.write(b",\n\"variants\": [\n")?;
+        }
+        if json_format == JsonFormat::NdJson {
+            output_stream.write(b"\n")?;
         }
         Ok(Self {
             output_stream,
             json_format,
+            write_state: WriteState::WroteMetadata,
         })
     }
 }
 
-/// TODO
-/// Work in progress
 impl<W: std::io::Write> output::MythWriter for JsonWriter<W> {
     fn add_myth(&mut self, myth: myth::Myth) -> error::Result<()> {
+        let myth_repr = json!(
+            {
+                "variant": &myth.variant,
+                "myth": &myth.annotations
+            }
+        );
+        let separator = match self.write_state {
+            WriteState::WroteMetadata => "".to_string(),
+            WriteState::WroteRecord if self.json_format == JsonFormat::Json => ",\n".to_string(),
+            WriteState::WroteRecord if self.json_format == JsonFormat::NdJson => "\n".to_string(),
+            _ => unreachable!(),
+        };
         match self.json_format {
             JsonFormat::Json => {
-                let variant_repr = serde_json::to_string_pretty(&myth.variant)?;
-                let one_line_per_record = true;
-                if one_line_per_record {
-                    self.output_stream.write(b"")?;
-                }
+                self.output_stream
+                    .write(format!("{}{:#}", separator, &myth_repr).as_bytes())?;
             }
-            JsonFormat::NdJson => {}
+            JsonFormat::NdJson => {
+                self.output_stream
+                    .write(format!("{}{}", separator, &myth_repr).as_bytes())?;
+            }
         }
+        self.write_state = match self.write_state {
+            WriteState::WroteMetadata => WriteState::WroteRecord,
+            WriteState::WroteRecord => WriteState::WroteRecord,
+        };
         Ok(())
     }
     fn batch_full(&self) -> bool {
@@ -81,12 +104,170 @@ impl<W: std::io::Write> output::MythWriter for JsonWriter<W> {
     }
     fn finalize(&mut self) -> error::Result<()> {
         if self.json_format == JsonFormat::Json {
-            self.output_stream.write(b"\n}")?;
+            self.output_stream.write(b"]\n}")?;
+        } else {
+            // Do nothing
         }
         self.output_stream.flush()?;
         Ok(())
     }
+
     fn write_batch(&mut self) -> error::Result<()> {
+        log::debug!("Calling write_batch on JsonWriter makes no sense!");
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::myth::Myth;
+
+    fn get_two_myths() -> (Myth, Myth) {
+        use crate::effect;
+        use crate::myth::{AnnotationMyth, Myth};
+        use crate::variant;
+
+        let mut annotation = AnnotationMyth::builder()
+            .source(b"test".to_vec())
+            .feature(b"gene".to_vec())
+            .name(b"gene1".to_vec())
+            .id(b"1111".to_vec());
+
+        annotation.add_effect(effect::Effect::GeneVariant);
+        annotation.add_effect(effect::Effect::ExonRegion);
+
+        let mut myth = Myth::from_variant(variant::Variant {
+            seqname: b"93".to_vec(),
+            position: 2036067340,
+            ref_seq: b"T".to_vec(),
+            alt_seq: b".".to_vec(),
+        });
+        myth.add_annotation(annotation.build().unwrap());
+
+        let mut annotation2 = AnnotationMyth::builder()
+            .source(b"test2".to_vec())
+            .feature(b"gene2".to_vec())
+            .name(b"gene51".to_vec())
+            .id(b"7777".to_vec());
+
+        annotation2.add_effect(effect::Effect::DisruptiveInframeDeletion);
+        annotation2.add_effect(effect::Effect::ExonRegion);
+
+        let mut myth2 = Myth::from_variant(variant::Variant {
+            seqname: b"21".to_vec(),
+            position: 1970,
+            ref_seq: b"C".to_vec(),
+            alt_seq: b"T".to_vec(),
+        });
+
+        myth2.add_annotation(annotation2.build().unwrap());
+        (myth, myth2)
+    }
+
+    #[test]
+    fn test_write_myth_json() {
+        use super::*;
+
+        use crate::output::MythWriter;
+
+        let (myth, myth2) = get_two_myths();
+
+        let output_stream: Vec<u8> = Vec::new();
+        let mut annot_writer = JsonWriter::new(output_stream, JsonFormat::Json).unwrap();
+
+        annot_writer.write_myth(myth).unwrap();
+        annot_writer.write_myth(myth2).unwrap();
+        annot_writer.finalize().unwrap();
+
+        eprintln!(
+            "{}",
+            std::str::from_utf8(&annot_writer.output_stream).unwrap()
+        );
+
+        assert_eq!(
+            std::str::from_utf8(&annot_writer.output_stream).unwrap(),
+            r#"{
+"metadata":
+{
+  "alt": "alternative sequence",
+  "chr": "chromosome name same ase original vcf",
+  "effect": "List of sequence ontology terms",
+  "feature": "type of feature affected by variant gene/transcript",
+  "id": "id of feature, same value of Id gff3 attributes",
+  "impact": "0: UNKOWN, 1:LOW, 2:MODIFIER, 3: MODERATE, 4:HIGH",
+  "name": "name of feature, same value of Name gff3 attributes",
+  "pos": "position of variant",
+  "ref": "reference sequence",
+  "source": "source of variant in gff3 file"
+},
+"variants": [
+{
+  "variant": {
+    "seqname": "93",
+    "position": 2036067340,
+    "ref_seq": "T",
+    "alt_seq": "."
+  },
+  "myth": [
+    {
+      "source": "test",
+      "feature": "gene",
+      "id": "1111",
+      "name": "gene1",
+      "effects": [
+        "GeneVariant",
+        "ExonRegion"
+      ],
+      "impact": "Modifier"
+    }
+  ]
+},
+{
+  "variant": {
+    "seqname": "21",
+    "position": 1970,
+    "ref_seq": "C",
+    "alt_seq": "T"
+  },
+  "myth": [
+    {
+      "source": "test2",
+      "feature": "gene2",
+      "id": "7777",
+      "name": "gene51",
+      "effects": [
+        "DisruptiveInframeDeletion",
+        "ExonRegion"
+      ],
+      "impact": "Moderate"
+    }
+  ]
+}]
+}"#
+        );
+    }
+
+    #[test]
+    fn test_write_myth_nd_json() {
+        use crate::output::{JsonFormat, JsonWriter, MythWriter};
+        let output_stream: Vec<u8> = Vec::new();
+        let mut annot_writer = JsonWriter::new(output_stream, JsonFormat::NdJson).unwrap();
+
+        let (myth, myth2) = get_two_myths();
+
+        annot_writer.write_myth(myth).unwrap();
+        annot_writer.write_myth(myth2).unwrap();
+        annot_writer.finalize().unwrap();
+
+        eprintln!(
+            "{}",
+            std::str::from_utf8(&annot_writer.output_stream).unwrap()
+        );
+        assert_eq!(
+            std::str::from_utf8(&annot_writer.output_stream).unwrap(),
+            r#"{"metadata":{"alt":"alternative sequence","chr":"chromosome name same ase original vcf","effect":"List of sequence ontology terms","feature":"type of feature affected by variant gene/transcript","id":"id of feature, same value of Id gff3 attributes","impact":"0: UNKOWN, 1:LOW, 2:MODIFIER, 3: MODERATE, 4:HIGH","name":"name of feature, same value of Name gff3 attributes","pos":"position of variant","ref":"reference sequence","source":"source of variant in gff3 file"}}
+{"variant":{"seqname":"93","position":2036067340,"ref_seq":"T","alt_seq":"."},"myth":[{"source":"test","feature":"gene","id":"1111","name":"gene1","effects":["GeneVariant","ExonRegion"],"impact":"Modifier"}]}
+{"variant":{"seqname":"21","position":1970,"ref_seq":"C","alt_seq":"T"},"myth":[{"source":"test2","feature":"gene2","id":"7777","name":"gene51","effects":["DisruptiveInframeDeletion","ExonRegion"],"impact":"Moderate"}]}"#
+        );
     }
 }
